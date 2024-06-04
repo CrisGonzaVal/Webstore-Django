2ab3cea187f5e66380d4cbf898901ab83a8c351f
CrisGonzaVal <cgonzalezvalenzuela4@gmail.com>
2024-06-03 23:06:38 -0400

    Solucion redireccion de vistas

--- app/urls.py
+++ app/urls.py
@@ -11,13 +11,13 @@ from.views import proveedores
 from.views import registro
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('carro/', carro, name="carro"),
-    path('catalogo/', catalogo, name="catalogo"),
-    path('contactanos/', contactanos, name="contactanos"),
-    path('login/', login, name="login"),
-    path('ofertas/', ofertas, name="ofertas"),
-    path('proveedores/', proveedores, name="proveedores"),
-    path('registro/', registro, name="registro"),
+    path('', home, name='home'),
+    path('carro/', carro, name='carro'),
+    path('catalogo/', catalogo, name='catalogo'),
+    path('contactanos/', contactanos, name='contactanos'),
+    path('login/', login, name='login'),
+    path('ofertas/', ofertas, name='ofertas'),
+    path('proveedores/', proveedores, name='proveedores'),
+    path('registro/', registro, name='registro'),
 ]
 
