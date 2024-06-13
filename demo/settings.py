a38105fed1a80a970b9137d57b4ab88e41496f7f
Angelitto616 <ang.henriquez@duocuc.cl>
2024-06-13 18:32:51 -0400

    pequeña modificacion

--- demo/settings.py
+++ demo/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'app.context_processors.carrito_context',  
+                'app.context_processor.carrito_context',  
             ],
         },
     },
