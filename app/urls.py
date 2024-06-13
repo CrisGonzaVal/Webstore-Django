3258ec696b3613204059dada11651b91813d8713
CrisGonzaVal <cgonzalezvalenzuela4@gmail.com>
2024-06-13 00:40:06 -0400

    se implementa los filtros funcionales del catalogo

--- app/urls.py
+++ app/urls.py
@@ -4,20 +4,16 @@ from django.urls import path
 from.views import home
 from.views import carro
 from.views import catalogo
-from.views import contactanos
 from.views import login
 from.views import ofertas
-from.views import proveedores
 from.views import registro
 
 urlpatterns = [
     path('', home, name='home'),
     path('carro/', carro, name='carro'),
     path('catalogo/', catalogo, name='catalogo'),
-    path('contactanos/', contactanos, name='contactanos'),
     path('login/', login, name='login'),
     path('ofertas/', ofertas, name='ofertas'),
-    path('proveedores/', proveedores, name='proveedores'),
     path('registro/', registro, name='registro'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render 
-from.models import Producto, Inventario
-from django.db.models import Sum
+from.models import Producto, Inventario, Categoria, Marca
+from django.db.models import Sum, Q
 
 
 # se crean las vistas
@@ -23,25 +23,47 @@ def catalogo(request):
     # Obtener todos los productos
     productos=Producto.objects.all()
 
+    # Obtener filtros de parámetros GET
+    marca = request.GET.get('marca')
+    categoria = request.GET.get('categoria')
+    precio = request.GET.get('precio')
+
+    # Aplicar filtros si están presentes
+    if marca:
+        productos = productos.filter(id_marca__nombre_m=marca)
+
+    if categoria:
+        productos = productos.filter(id_categoria__nombre=categoria)
+
+    if precio == 'asc':
+        productos = productos.order_by('valor')
+    elif precio == 'desc':
+        productos = productos.order_by('-valor')
+
     # Calcular la cantidad total de cada producto
     # consulta usando python que es = select cantidad AS sum(cantidad_total) FROM Inventario WHERE id_producto = producto.id_producto
     for producto in productos:
         cantidad_total = Inventario.objects.filter(id_producto=producto).aggregate(total=Sum('cantidad'))['total']
         producto.cantidad_total = cantidad_total if cantidad_total is not None else 0
 
+
+        # Obtener listas para los filtros
+    categorias = Categoria.objects.all()
+    marcas = Marca.objects.all()
+
     # Pasar los productos con la cantidad total calculada al contexto
     data={
-       'productos': productos
+       'productos': productos,
+       'categorias': categorias,
+       'marcas': marcas
     }
     return render(request, 'app/catalogo.html',data)  
 
-def contactanos(request):
-    return render(request, 'app/contactanos.html')  
+
 
 def ofertas(request):
     return render(request, 'app/ofertas.html')  
 
-def proveedores(request):
-    return render(request, 'app/proveedores.html')   
+
 
 
