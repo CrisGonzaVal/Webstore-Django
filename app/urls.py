a0ee52d77b3868db01e029984c1300a9f5ee72a6
Angelitto616 <ang.henriquez@duocuc.cl>
2024-06-12 22:48:40 -0400

    Agregue las urs de las  funciones y una lista en el catalogo O.o

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,10 @@
 #se crean los urls de cada vista (html)
 
 from django.urls import path
-from.views import home
-from.views import carro
-from.views import catalogo
-from.views import contactanos
-from.views import login
-from.views import ofertas
-from.views import proveedores
-from.views import registro
+from .views import (
+    home, carro, catalogo, contactanos, login, ofertas, proveedores, registro,
+    agregar_producto, eliminar_producto, restar_producto, limpiar_carrito
+)
 
 urlpatterns = [
     path('', home, name='home'),
@@ -19,5 +15,9 @@ urlpatterns = [
     path('ofertas/', ofertas, name='ofertas'),
     path('proveedores/', proveedores, name='proveedores'),
     path('registro/', registro, name='registro'),
+    path('agregar/<int:producto_id>/', agregar_producto, name='agregar_producto'),
+    path('eliminar/<int:producto_id>/', eliminar_producto, name='eliminar_producto'),
+    path('restar/<int:producto_id>/', restar_producto, name='restar_producto'),
+    path('limpiar/', limpiar_carrito, name='limpiar_carrito'),
 ]
 
